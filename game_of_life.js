352bb967a033855461640c67abfb56bc007ba7b5
Adi Joost <adi.joost@gmail.com>
2021-10-03 17:40:42 +0200

    basic functionality added - boarders are Dead-Zone

--- game_of_life.js
+++ game_of_life.js
@@ -8,11 +8,68 @@ class Tile {
 		this.y = y;
 		this.container = container
 		this.is_alive = is_alive;
+		this.will_be_alive = is_alive;
+		if(x == 0 || y == 0 || x == x_size - 1 || y == y_size - 1){
+			this.is_border = true;
+		} else {
+			this.is_border = false;
+		}
 	}
 
 	resurrect(){
 		this.is_alive = true;
 		this.container.classList.add("isAlive");
+	}
+
+	die(){
+		this.container.classList.remove("isAlive");
+	}
+
+	activate_new_state(){
+		console.log(this);
+		if (this.is_alive == this.will_be_alive){
+			return;
+		}
+		this.is_alive = !this.is_alive;
+		if(this.is_alive == true){
+			this.resurrect();
+		} else {
+			this.die();
+		}
+	}
+
+	get_new_state(){
+		if (this.is_border == true){
+			this.will_be_alive = false;
+			return false;
+		}
+
+		let neighbours = 0
+		for (let y = this.y - 1; y < this.y + 2; y++){
+			for (let x = this.x - 1; x < this.x + 2; x++){
+				if (map_tracker[y][x].is_alive == true){
+					neighbours += 1;
+				}
+			}
+		}
+
+		/*Checks if neighbours = 2 or 3 and sets will_be_alive for next generation*/
+		if (this.is_alive == true){
+			neighbours -= 1;
+			if (neighbours > 1 && neighbours < 4){
+				this.will_be_alive = true;
+			} else {
+				this.will_be_alive = false;
+			}
+		} else {
+			if (neighbours == 3){
+				this.will_be_alive = true;
+			}else{
+				this.will_be_alive = false;
+			}
+		}
+
+
 	}
 }
 
@@ -20,6 +77,10 @@ class Tile {
 window.addEventListener("load", function(){
 	init_field();
 	shuffle_alive(spawn_rate=30);
+	let next_gen = document.getElementById('next_gen');
+	next_gen.addEventListener("click", function(){
+		get_next_state();
+	}, false)
 }, false)
 
 function init_field(){
@@ -42,8 +103,6 @@ function init_field(){
 		map.appendChild(row);
 		map_tracker.push(map_row);
 	}
-
-	
 }
 
 function shuffle_alive(spawn_rate = 30){
@@ -53,6 +112,19 @@ function shuffle_alive(spawn_rate = 30){
 			if (spawn_rate > randInt){
 				map_tracker[y][x].resurrect();
 			}
+		}
+	}
+}
+
+function get_next_state(){
+	for (let y = 0; y < y_size; y++){
+		for(let x = 0; x < x_size; x++){
+			map_tracker[y][x].get_new_state();
+		}
+	}
+	for (let y = 0; y < y_size; y++){
+		for(let x = 0; x < x_size; x++){
+			map_tracker[y][x].activate_new_state();
 		}
 	}
 }
